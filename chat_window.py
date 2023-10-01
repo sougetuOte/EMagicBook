bd46324f3172a649466fc0932c22a24a0d8b9e36
sougetuOte <35713865+sougetuOte@users.noreply.github.com>
2023-10-02 08:46:46 +0900

    テキストエリア、ボックス背景出来ず。

--- chat_window.py
+++ chat_window.py
@@ -37,6 +37,11 @@ class ChatWindow(tk.Toplevel):
         self.canvas.place(x=self.FRAME_OFFSET, y=self.FRAME_OFFSET) 
 
         # オブジェクトの作成
+        #text_bg_image = Image.open("images/text_area.png")
+        #text_area_image = text_bg_image.resize((610, 600))  # 画像をリサイズ
+        #self.text_area_bg = ImageTk.PhotoImage(text_area_image)
+        #text_box_image = text_bg_image.resize((610, 100))  # 画像をリサイズ
+        #self.text_box_bg = ImageTk.PhotoImage(text_box_image)
         self.create_text_area()
         self.create_text_box()
         self.create_command_button()
@@ -45,6 +50,7 @@ class ChatWindow(tk.Toplevel):
         # ウィンドウ位置の更新
         self.update_position()
 
+    '''
     def create_text_area(self):
         self.text_area = scrolledtext.ScrolledText(self, wrap=tk.WORD, width=50, height=10, bg=self.TEXT_BG_COLOR)  # 背景色を黄土色に変更
         self.text_area.place(x=30, y=30, width=640, height=600)
@@ -52,7 +58,15 @@ class ChatWindow(tk.Toplevel):
     def create_text_box(self):
         self.text_box = scrolledtext.ScrolledText(self, wrap=tk.WORD, width=50, height=5, bg=self.TEXT_BG_COLOR)  # 背景色を黄土色に変更
         self.text_box.place(x=30, y=640, width=640, height=100)
-
+    '''
+    def create_text_area(self):
+        self.text_area = scrolledtext.ScrolledText(self, wrap=tk.WORD, width=80, height=20, bg="khaki") 
+        self.text_area_window = self.canvas.create_window(30, 30, window=self.text_area, anchor=tk.NW, width=640, height=600)
+    
+    def create_text_box(self):
+        self.text_box = scrolledtext.ScrolledText(self, wrap=tk.WORD, width=40, height=5, bg="khaki")
+        self.text_box_window = self.canvas.create_window(30, 640, window=self.text_box, anchor=tk.NW, width=640, height=100)
+    
     def create_command_button(self):
         self.command_button = tk.Button(self, text="Enter", command=self.send_message)
         self.command_button.place(x=580, y=750, width=80, height=30)
